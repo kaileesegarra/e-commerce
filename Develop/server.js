ed9b61c05f5061699862e517b769ebb3bd78816c
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-26 09:34:23 -0400

    attempt to debug

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Import and use routes
+const routes = require('./routes');
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`Server running on port ${PORT}`);
 });
